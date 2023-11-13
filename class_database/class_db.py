a72b8aecaa4d30f16bb0f5314bd8e00c049ef16c
Atij Mahesh <atijmahesh914@gmail.com>
2023-11-12 18:56:05 -0800

    First iteration of classes Database
    First iteration of Class Database
    
    Using Flask+Python+SQLite, overall framework for Class DB implemented + tested. Need to rework classes to better use data.

--- class_database/class_db.py
+++ class_database/class_db.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ucla_classes.db'
@@ -11,9 +13,6 @@ class Class(db.Model):
     name_description = db.Column(db.String(255))
     name = db.Column(db.String(255))
 
-# Create the database tables
-db.create_all()
-
 # API endpoint to get classes
 @app.route('/api/classes', methods=['GET'])
 def get_classes():
@@ -22,4 +21,36 @@ def get_classes():
     return jsonify({'classes': class_list})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Move database-related code inside the application context
+    with app.app_context():
+        # Clear existing data and create tables
+        db.drop_all()
+        db.create_all()
+
+        # Load JSON data using an absolute path
+        json_path = os.path.join(os.path.dirname(__file__), '../scrape/ucla_classes.json')
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+        
+        # Insert or update data into the database
+        for department, classes in data.items():
+            for class_info in classes:
+                existing_class = db.session.get(Class, class_info['id'])
+                if existing_class:
+                    # Update existing record
+                    existing_class.name_description = class_info['name_description']
+                    existing_class.name = class_info['name']
+                else:
+                    # Insert new record
+                    new_class = Class(
+                        name_description=class_info['name_description'],
+                        id=class_info['id'],
+                        name=class_info['name']
+                    )
+                    db.session.add(new_class)
+
+        # Commit changes to the database
+        db.session.commit()
+
+    # Run the application
+    app.run(debug=False)
