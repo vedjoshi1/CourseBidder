89c6875913bb12580041381b0717e7fa360b545e
vedjoshi1 <66143395+vedjoshi1@users.noreply.github.com>
2023-12-02 14:04:00 -0800

    Fixed getListing

--- cbreact/serverStuff.js
+++ cbreact/serverStuff.js
@@ -303,19 +303,25 @@ app.post("/makeListing", body('departmentId'), body('price').isFloat({min: 0}), 
 
 
 app.get("/getListings", query('departmentId'), async (req, res) => {
+
+  
     const result = validationResult(req);
     if(result.isEmpty()) {
       try {
         await tryMongooseConnection();
       } catch (error) {
-        res.status(500).json({errors: ["could not connect to mongodb"]}).send()
+        res.status(501).json({errors: ["could not connect to mongodb"]}).send()
         console.log(error)
         return;
       }
-      const {departmentId} = matchedData(req);
 
+      
+      const { departmentId } = req.body;
+      console.log('Extracted departmentId:', departmentId);
+      
       try {
         const classes = await classCollection.findOne({departmentId: departmentId}).lean()
+      //  console.log(classes);
         string_classes = classes.listings.slice(0, Math.min(classes.listings.length, 1000)).map((course) => {
           return {
             email: course.email,
@@ -370,31 +376,43 @@ app.get("/mainpage", (req, res) => {
 
 
 
-app.post("/getuser", async (req, res) => {
-  try {
-    const { userId } = req.body; // Assuming the user ID is sent in the request body
-
-    // Fetch user from the database
-    const user = await userCollection.findById(userId);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    // Update user profile if new data is provided
-    const { fullname, email, password } = req.body;
-    if (fullname) user.name = fullname;
-    if (email) user.email = email;
-    if (password) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      user.hashedPassword = hashedPassword;
-    }
+app.get("/getuser", async (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    try {
+        await tryMongooseConnection();
+      } catch (error) {
+        res.status(500).json({errors: ["could not connect to mongodb"]}).send()
+        console.log(error)
+        return;
+      }
+      try {
+        const user = await getUserFromCookie(req?.cookies?.session)
+        const { fullname, email, password } = req.body;
+        if (fullname) user.name = fullname;
+        if (email) user.email = email;
+        if (password) {
+          const hashedPassword = await bcrypt.hash(password, 10);
+          user.hashedPassword = hashedPassword;
+       }
     // Save the updated user profile
-    const updatedUser = await user.save();
+        const updatedUser = await user.save();
+        res.status(201).json({ message: 'User profile updated successfully', user: updatedUser });
 
-    res.status(201).json({ message: 'User profile updated successfully', user: updatedUser });
-  } catch (error) {
-    console.error('Error updating user profile:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+      } catch (error) {
+        res.status(400).send({errors: ['could not get user email from cookie!']}) 
+        console.log(error);
+      }
+
+
+
+
+
+  }else{
+
+    res.status(422).json({errors: result.array() }).send();
+    console.log("inputs failed validation")
+
   }
 });
 
