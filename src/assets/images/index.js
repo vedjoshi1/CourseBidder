471cc8e679e6067caeaec481da266e44deef686c
Anish Pal <anishmpal@gmail.com>
2023-12-04 23:21:49 -0800

    remove cbreact and misc dirs

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,7 +2,7 @@ import logo from "./cblogo.png";
 import art from "./cbart.png";
 import logoLight from "./cblogo_light.png";
 import emptyListings from "../images/emptyListings.png";
-import emptyCart from "./EmptyCart.png"
+import emptyCart from "./emptyCart.png"
 
 export {
   logo,
